bf5b1854fab5e6e3e6476ebc4cfee710e703321f
Renzo Canepa <rcanepag@gmail.com>
2018-01-22 11:31:10 -0300

    add users service

---
+++ users-service/project/config.py
@@ -0,0 +1,20 @@
+class BaseConfig(object):
+    """Base configuration"""
+    DEBUG = False
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    DEBUG = True
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    DEBUG = True
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    DEBUG = False
